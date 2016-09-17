5e2c4b5055d9be24636df6b1833e0176a6499d33
kekeh <kea@live.fi>
2016-09-17 16:42:29 +0300

    Demo page app updated to use Angular 2 final version.

---
+++ config/webpack.build.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+
+    entry: {
+        'polyfills': './src/polyfills.ts',
+        'vendor': './src/vendor.ts',
+        'app': './src/main.ts'
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                exclude: [/\.spec\.ts$/],
+                loader: 'ts'
+            },
+            {
+                test: /\.html$/,
+                loader: 'raw'
+            },
+            {
+                test: /\.css$/,
+                loader: "to-string-loader!css-loader"
+            }
+        ]
+    },
+
+    output: {
+        path: path.resolve('dist'),
+        publicPath: '/mydatepicker/dist/',
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    htmlLoader: {
+        minimize: false // workaround for ng2
+    },
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({ output: {comments: false}, mangle: { screw_ie8 : true, keep_fnames: true} }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'config/index.build.template.ejs'
+        })
+    ]
+};
