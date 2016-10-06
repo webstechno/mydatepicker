83a2b7a6d4da05435322a089b56891b60cf30e78
kekeh <kea@live.fi>
2016-10-06 20:19:40 +0300

    Demo page app updated.

--- src/app/sample-date-picker-module.ts
+++ src/app/sample-date-picker-module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MyDatePickerApp }  from './sample-date-picker-app';
 import { SampleDatePickerNormal }  from './components/sample-date-picker-normal';
 import { SampleDatePickerInline }  from './components/sample-date-picker-inline';
-import { MyDatePickerModule } from 'mydatepicker/src/my-date-picker/my-date-picker.module';
+import { MyDatePickerModule } from 'mydatepicker';
 
 @NgModule({
     imports:      [ BrowserModule, MyDatePickerModule ],
