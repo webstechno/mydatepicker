a1747a2b8902ea0d9ab3049a8984dea7579c6fef
kekeh <kea@live.fi>
2017-04-20 18:10:05 +0300

    Demo page app updated.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -6,6 +6,4 @@ if (process.env.ENV === 'production') {
     // Production
 } else {
     // Development
-    Error['stackTraceLimit'] = Infinity;
-    require('zone.js/dist/long-stack-trace-zone');
 }
