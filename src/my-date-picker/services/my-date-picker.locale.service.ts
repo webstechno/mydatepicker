c26f6453c63638a4b1281125bce1482c11a561c9
Mohamed-Gamal2015 <medo.gemy@yahoo.com>
2017-05-28 19:13:33 +0200

    Adding arabic locale
    Showing  day Labels and month Labels in Arabic

--- src/my-date-picker/services/my-date-picker.locale.service.ts
+++ src/my-date-picker/services/my-date-picker.locale.service.ts
@@ -4,6 +4,15 @@ import { IMyLocales, IMyOptions } from "../interfaces/index";
 @Injectable()
 export class LocaleService {
     private locales: IMyLocales = {
+        "ar":{
+            dayLabels: { su: "الأحد", mo: "الاثنين", tu: "الثلاثاء", we: "الاربعاء", th: "الخميس", fr: "الجمعة", sa: "السبت" },
+            monthLabels: { 1: "يناير", 2: "فبراير", 3: "مارس", 4: "ابريل", 5: "مايو", 6: "يونيو", 7: "يوليو", 8: "أغسطس", 9: "سبتمبر", 10: "أكتوبر", 11: "نوفمبر", 12: "ديسمبر" },
+            dateFormat: "yyyy-mm-dd",
+            todayBtnTxt: "اليوم",
+            firstDayOfWeek: "sa",
+            sunHighlight: true,
+            
+        },
         "en": {
             dayLabels: {su: "Sun", mo: "Mon", tu: "Tue", we: "Wed", th: "Thu", fr: "Fri", sa: "Sat"},
             monthLabels: { 1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec" },
